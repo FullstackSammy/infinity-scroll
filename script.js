ea6af51237e798faf6844f156be90698921b7d9d
samzzii <nasstromsamuel@icloud.com>
2022-11-11 15:31:43 +0100

    Add constants and empty photosArray

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
+// constants
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+
 
 // Unsplash API
 const count = 10;
 const apiKey = '_A407AN1t8fZui_UZWtK8UfZtXjKmGWd25ZWQTW7apc';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+let photosArray = [];
+
 // Get photos from Unsplash API
 async function getPhotos() {
     try {
         const response = await fetch(apiUrl);
-        const data = await response.json();
-        console.log(data);
+        photosArray = await response.json();
+        console.log(photosArray);
 } catch (error) {
         
     }
